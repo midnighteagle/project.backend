fe2c010c48c02724a15b9a91fce07740675ed7e3
midnighteagle <chiranjeevivaston@gmail.com>
2026-03-05 14:11:33 +0530

    fix: correct import path for ApiResponse in video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {
   deleteFile,
